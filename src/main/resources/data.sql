4b4db7ef22d93169fb6cde8d52dc228ef33fe188
Anileve <anileve.mv@gmail.com>
2025-01-06 16:25:18 +0300

    Adding files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living) VALUES ('John', 'Doe', 30, '123-456-7890', 'Moscow');
-INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living) VALUES ('Jane', 'Smith', 25, '098-765-4321', 'Moscow');
-INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living) VALUES ('Alice', 'Johnson', 35, '456-789-0123', 'New York');
+INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living)
+VALUES ('John', 'Doe', 30, '123-456-7890', 'Moscow');
+INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living)
+VALUES ('Jane', 'Smith', 25, '098-765-4321', 'Moscow');
+INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living)
+VALUES ('Alice', 'Johnson', 35, '456-789-0123', 'New York');
