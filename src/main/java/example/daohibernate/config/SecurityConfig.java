860c3ade6ae44e2dbc0fe58b70be1f6fce847f82
Anileve <anileve.mv@gmail.com>
2025-01-25 12:40:55 +0300

    Adding files

--- src/main/java/example/daohibernate/config/SecurityConfig.java
+++ src/main/java/example/daohibernate/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                .requestMatchers(HttpMethod.GET, "/public")
+                .requestMatchers(HttpMethod.GET, "/persons/public")
                 .permitAll()
                 .anyRequest()
                 .authenticated()); // Все остальные требуют авторизации
